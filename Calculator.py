9555e471f35e01df1727f36f347f3b85bb87543e
Madhav Krishna <madhavkrishna1818@gmail.com>
2025-04-22 23:28:16 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -29,7 +29,7 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1 / n2
 
-operators = {"+" : add ,
+operators ={"+" : add ,
              "-" : subtract ,
              "*" : multiply ,
              "/" : divide}
